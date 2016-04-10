4f6f50d939ed8899a0fc1de5294561d3698e9d24
Grzegorz Abram <grzegorz.abram@jcommerce.pl>
2016-04-10 13:45:10 +0200

    added database scripts

--- src/main/resources/db/sql/create-db.sql
+++ src/main/resources/db/sql/create-db.sql
@@ -1,19 +1,19 @@
-CREATE TABLE users (
-  username VARCHAR(500) NOT NULL,
+CREATE TABLE users(
+  username VARCHAR(50) NOT NULL,
   password VARCHAR(500) NOT NULL,
-  enabled boolean,
+  enabled BOOLEAN NOT NULL,
   PRIMARY KEY (username)
 );
 
-CREATE TABLE authorities (
-  username VARCHAR(500) NOT NULL,
-  authority VARCHAR(500) NOT NULL,
-  PRIMARY KEY (username)
+CREATE TABLE authorities(
+  username VARCHAR(50) NOT NULL,
+  authority VARCHAR(50) NOT NULL,
+  UNIQUE INDEX (username, authority)
 );
 
-CREATE TABLE wallets (
-  username VARCHAR(500) NOT NULL,
+CREATE TABLE wallets(
+  username VARCHAR(50) NOT NULL,
   currency VARCHAR(5) NOT NULL,
-  amount INT,
+  amount INT NOT NULL,
   PRIMARY KEY (username, currency)
 );
--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,1 +1,2 @@
-INSERT INTO users (username, password) VALUES ('user1', '123');
+INSERT INTO users (username, password, enabled) VALUES ('user', '$2a$10$IzWi1i14KuUu239KDpGStuuTi67bNIfzgrttFoK7o0aynENIOGIqa', true);
+INSERT INTO authorities (username, authority) VALUES ('user', 'USER');
